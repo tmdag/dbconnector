bc6348abfb905eb3565ef8ac0871c5431bd8d793
Albert Szostkiewicz <tmdag@tmdag.com>
2019-05-25 14:22:56 -0700

    added setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dbconnector',
+      version='0.1',
+      description='The dbconnector joke in the world',
+      url='http://github.com/tmdag/dbconnector',
+      author='Albert Szostkiewicz',
+      author_email='tmdag@tmdag.com',
+      license='MIT',
+      packages=['dbconnector'],
+      zip_safe=False)
